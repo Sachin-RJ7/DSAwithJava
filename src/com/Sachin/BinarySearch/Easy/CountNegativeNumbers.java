f8e4c31064b529c53467682fc266eb1854745ecf
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-10-13 07:53:58 +0500

    | added question detail |

--- src/com/Sachin/BinarySearch/Easy/CountNegativeNumbers.java
+++ src/com/Sachin/BinarySearch/Easy/CountNegativeNumbers.java
@@ -1,5 +1,18 @@
 package com.Sachin.BinarySearch.Easy;
 
+
+//      https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
+//      About Question:
+
+//      Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number
+//      of negative numbers in grid.
+
+//      Example :
+//      Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+//      Output: 8
+//      Explanation: There are 8 negatives number in the matrix.
+
 public class CountNegativeNumbers {
     public static void main(String[] args) {
         int[][] arr = {
