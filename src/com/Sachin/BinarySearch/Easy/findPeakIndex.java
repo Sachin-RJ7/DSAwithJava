d276f854e326d1db306b84c2afd2eba72cbdaa80
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-10-13 07:58:56 +0500

    | added question detail |

--- src/com/Sachin/BinarySearch/Easy/findPeakIndex.java
+++ src/com/Sachin/BinarySearch/Easy/findPeakIndex.java
@@ -1,7 +1,24 @@
 package com.Sachin.BinarySearch.Easy;
 
-public class findPeakIndex {
-    public static void main(String[] args) {
+//  https://leetcode.com/problems/find-peak-element/
+
+//      About Question:
+
+//      A peak element is an element that is strictly greater than its neighbors.
+//      Given a 0-indexed integer array nums, find a peak element, and return its index.
+//      If the array contains multiple peaks, return the index to any of the peaks.
+//      You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered
+//      to be strictly greater than a neighbor that is outside the array.
+//      You must write an algorithm that runs in O(log n) time.
+
+//      Example :
+//      Input: nums = [1,2,3,1]
+//      Output: 2
+//      Explanation: 3 is a peak element and your function should return the index number 2.
+
+
+    public class findPeakIndex {
+        public static void main(String[] args) {
         int[] arr = {0,10,5,2};
         System.out.println(peakIndexInMountainArray(arr));
     }
