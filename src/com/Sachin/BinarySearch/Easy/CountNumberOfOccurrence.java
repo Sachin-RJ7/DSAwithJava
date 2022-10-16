83b43cbc2e197f6c3ce38f33da7a280be96556bb
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-10-16 10:33:12 +0500

    added question details

--- src/com/Sachin/BinarySearch/Easy/CountNumberOfOccurrence.java
+++ src/com/Sachin/BinarySearch/Easy/CountNumberOfOccurrence.java
@@ -1,5 +1,20 @@
 package com.Sachin.BinarySearch.Easy;
 
+
+//  https://practice.geeksforgeeks.org/problems/number-of-occurrence2259/1
+
+//  Question:-
+//  Given a sorted array Arr of size N and a number X, you need to find the number of occurrences of X in Arr.
+
+//  Example 1:
+//  Input:
+//  N = 7, X = 2
+//  Arr[] = {1, 1, 2, 2, 2, 2, 3}
+//  Output: 4
+//  Explanation: 2 occurs 4 times in the given array.
+
+
+
 public class CountNumberOfOccurrence {
     public static void main(String[] args) {
         int[] arr = {4, 4, 8, 8, 8, 15, 16, 23, 23, 42};
