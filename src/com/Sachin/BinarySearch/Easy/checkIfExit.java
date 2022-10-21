49efcdd09a7596a9ca8037ae42ff0f902df4e971
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-10-21 09:34:11 +0500

    | check if exit (brute-force code) |

---
+++ src/com/Sachin/BinarySearch/Easy/checkIfExit.java
@@ -0,0 +1,31 @@
+package com.Sachin.BinarySearch.Easy;
+
+public class checkIfExit {
+    public static void main(String[] args) {
+        int[] arr = {-20, 8, -6, -14, 0, -19, 14, 4};
+        System.out.println(checkExitOrNot(arr));
+    }
+
+    public static boolean checkExitOrNot(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            int temp = arr[i] * 2;
+            for(int j = 0; j < arr.length; j++){
+                if(i == j) {
+                    j++;
+                }
+                while(i == arr.length -1) {
+                    for(int k = 0; k < arr.length-1; k++){
+                        if(temp == arr[k]) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                if(temp == arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
