cee4014069497f04012049ab3514e52db9844ed7
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-11-13 20:12:20 +0500

    find interval

---
+++ src/com/Sachin/BinarySearch/Medium/findInterval.java
@@ -0,0 +1,23 @@
+package com.Sachin.BinarySearch.Medium;
+
+public class findInterval {
+    public static void main(String[] args) {
+
+    }
+
+//    public static int[] findRightInterval(int[][] intervals) {
+//        int[] arr ={ -1 };
+//        for (int i = 0; i < intervals.length; i++) {
+//            for (int j = 0; j < intervals[i].length; j++) {
+//                int start = intervals[i][j];
+//                int last = intervals[i].length-1;
+//                if (last >= start) {
+//                    arr[i] = start;
+//                }
+//                else {
+//
+//                }
+//            }
+//        }
+//    }
+}
