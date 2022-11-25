b9d437a315c6140a5d045c2d827e4750436ab80a
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-11-25 08:49:04 +0500

    | solution with predefined function |

--- src/com/Sachin/BinarySearch/Hard/MedianOfTwoSortedArray.java
+++ src/com/Sachin/BinarySearch/Hard/MedianOfTwoSortedArray.java
@@ -16,11 +16,14 @@ package com.Sachin.BinarySearch.Hard;
 // Output: 2.50000
 // Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 
+import java.util.Arrays;
+
 public class MedianOfTwoSortedArray {
     public static void main(String[] args) {
         int[] nums1 = {1, 2};
         int[] nums2 = {3, 4};
         System.out.println(findMedianSortedArrays(nums1,nums2));
+        System.out.println(findMedianSortedArrays2(nums1,nums2));
     }
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1=nums1.length;
@@ -74,4 +77,48 @@ public class MedianOfTwoSortedArray {
 
 
     }
+
+    // Simple approach....My own approach
+    public static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+
+        // determines length of firstArray
+        int a1 = nums1.length;
+
+        // determines length of secondArray
+        int b1 = nums2.length;
+
+        // resultant array size
+        int c1 = a1 + b1;
+
+        // create the resultant array
+        int[] c = new int[c1];
+
+        // using the pre-defined function arraycopy
+        System.arraycopy(nums1, 0, c, 0, a1);
+        System.arraycopy(nums2, 0, c, a1, b1);
+
+        Arrays.sort(c);
+
+        // prints the resultant array
+        System.out.println(Arrays.toString(c));
+
+
+        int start = 0;
+        int end = c.length-1;
+
+        int mid = start + (end - start)/ 2;
+
+        double sum = 0.0;
+
+        if(c.length % 2 == 0) {
+            sum = (c[mid] + c[mid+1])/2.0;
+            return sum;
+        }
+        else {
+            sum = c[mid];
+            return sum;
+        }
+
+    }
+
 }
