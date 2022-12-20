557c74aae0e590823f50b3b79038e33b814d78e6
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2022-12-20 07:22:14 +0500

    done cyclic sort

---
+++ src/com/Sachin/Sorting/CyclicSort.java
@@ -0,0 +1,33 @@
+package com.Sachin.Sorting;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // cyclic sort ...
+    public static void cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length){
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]){
+                swap(arr, i, correctIndex);
+            }
+            else {
+                i++;
+            }
+        }
+    }
+
+    // swapping function ...
+    private static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+}
