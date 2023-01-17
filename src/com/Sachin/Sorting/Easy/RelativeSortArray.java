82a2b80758efb37328fecb0c6acd956aacd73919
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-01-17 21:24:05 +0500

    relative sort array

---
+++ src/com/Sachin/Sorting/Easy/RelativeSortArray.java
@@ -0,0 +1,15 @@
+package com.Sachin.Sorting.Easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RelativeSortArray {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        Map<Integer,Integer> map = new  HashMap<>();
+
+    }
+}
