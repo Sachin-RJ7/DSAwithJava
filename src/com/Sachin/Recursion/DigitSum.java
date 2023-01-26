ac5c42d4cb5971225bc2f714290d0182f0821115
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-01-26 10:20:57 +0500

    find sum of digit

---
+++ src/com/Sachin/Recursion/DigitSum.java
@@ -0,0 +1,16 @@
+package com.Sachin.Recursion;
+
+public class DigitSum {
+    public static void main(String[] args) {
+        int ans = sumOfDigit(1234);
+        System.out.println(ans);
+    }
+
+    public static int sumOfDigit(int n){
+        if (n == 0) {
+            return 0;
+        }
+        return  n % 10 + sumOfDigit(n / 10);
+    }
+
+}
