7b2a03738035e9b67ce1db477ea059496d919c31
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-01-29 10:07:57 +0500

    find target

---
+++ src/com/Sachin/Recursion/LinearSearch.java
@@ -0,0 +1,80 @@
+package com.Sachin.Recursion;
+
+import java.util.ArrayList;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 1, 8, 9,8, 22, 8, 12, 14};
+        int target = 8;
+        System.out.println(find(arr, target, 0));
+        System.out.println(findTarget(arr, target, 0));
+        System.out.println(findTargetLast(arr, target, arr.length-1));
+        
+        findAllTarget(arr, target, 0);
+        System.out.println(list);
+
+        ArrayList<Integer> ans = findAllTarget2(arr, target, 0, new ArrayList<>());
+        System.out.println(ans);
+    }
+
+    public static boolean find(int[] arr, int target, int index){
+        if (index == arr.length){
+            return false;
+        }
+
+        return arr[index] == target || find(arr, target, index + 1);
+    }
+
+    public static int findTarget(int[] arr, int target, int index) {
+
+        if (index == arr.length){
+            return -1;
+        }
+
+        if (arr[index] == target){
+            return index;
+        }
+
+        return  findTarget(arr, target, index+1);
+    }
+
+    // finding the target from the last element
+    public static int findTargetLast(int[] arr, int target,int index ) {
+        if (index == -1){
+            return -1;
+        }
+
+        if (arr[index] == target){
+            return index;
+        }
+
+        return findTargetLast(arr, target, --index);
+    }
+
+
+    static ArrayList<Integer> list = new ArrayList<>();
+    public static void findAllTarget(int[] arr, int target, int index) {
+        if (index == arr.length){
+            return;
+        }
+
+        if (arr[index] == target){
+            list.add(index);
+        }
+
+        findAllTarget(arr, target, index+1);
+    }
+
+    public static ArrayList<Integer> findAllTarget2(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if (index == arr.length){
+            return list;
+        }
+
+        if (arr[index] == target){
+            list.add(index);
+        }
+
+        return findAllTarget2(arr, target, index+1, list);
+    }
+
+}
