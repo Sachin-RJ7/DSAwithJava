5c05fc7b5f18b511c37daf543b489e6fae747de7
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-02-01 09:34:27 +0500

    bubble sort

---
+++ src/com/Sachin/Recursion/BubbleSort.java
@@ -0,0 +1,30 @@
+package com.Sachin.Recursion;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 6, 7, 8, 9, 3};
+        bubbleSort(arr,arr.length - 1, 0);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr, int r, int c){
+        if (r == 0)
+            return;
+
+        if(c < r){
+            if (arr[c] > arr[c + 1]){
+                // swap
+                int temp = arr[c];
+                arr[c] = arr[c + 1];
+                arr[c + 1] = temp;
+            }
+
+            bubbleSort(arr, r, c + 1);
+        }
+        else{
+            bubbleSort(arr, r - 1, 0);
+        }
+    }
+}
