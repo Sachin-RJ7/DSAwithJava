311df819ea6ceb7f4762535d743e1c47c1abdab6
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-02-16 07:19:17 +0500

    find power of x

--- src/com/Sachin/Recursion/Medium/Power.java
+++ src/com/Sachin/Recursion/Medium/Power.java
@@ -2,19 +2,35 @@ package com.Sachin.Recursion.Medium;
 
 public class Power {
     public static void main(String[] args) {
-        System.out.println(pow(2, 5));
+        System.out.println(pow(2.00000, 10));
+        System.out.println(myPow(2.00000, 10));
     }
 
-    public static double pow(double x, long n){
-        if(n==0 || x==1)
-            return 1;
-        if(x==0)
-            return 0;
+    public static double pow(double x, int n){
+        if(n < 0){
+            n = -n;
+            x = 1 / x;
+        }
 
-        if(n<0)
-            return pow(1/x,Math.abs(n));
+        double pow = 1;
 
-        return (n%2==0 ? pow(x*x,n/2) : x*pow(x*x,n/2));
+        while(n != 0){
+            if((n & 1) != 0){
+                pow *= x;
+            }
 
+            x *= x;
+            n >>>= 1;
+
+        }
+
+        return pow;
+    }
+
+    public static double myPow(double x, int n){
+        if (n==0) return 1;
+        else if (n%2==0) return myPow(x*x, n/2);
+        else if (n%2==1) return x*myPow(x, n-1);
+        return 1/myPow(x, -n);
     }
 }
