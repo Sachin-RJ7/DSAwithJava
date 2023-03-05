7bfa8f44264048fc1234b6b38f4e065338a11b6d
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-03-05 23:58:04 +0500

    Added question

--- src/com/Sachin/Arrays/Easy/RemoveDuplicates.java
+++ src/com/Sachin/Arrays/Easy/RemoveDuplicates.java
@@ -1,5 +1,7 @@
 package com.Sachin.Arrays.Easy;
 
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
 public class RemoveDuplicates {
     public static void main(String[] args) {
         int[] arr = { 0, 0, 1, 1, 2, 2, 3};
