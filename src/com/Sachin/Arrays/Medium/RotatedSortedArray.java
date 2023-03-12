6cfdac159d868e2ce44fec57de2b13e5494f8455
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-03-12 23:39:30 +0500

    find in rotated sorted array

---
+++ src/com/Sachin/Arrays/Medium/RotatedSortedArray.java
@@ -0,0 +1,70 @@
+package com.Sachin.Arrays.Medium;
+
+public class RotatedSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(search(arr, target));
+    }
+
+    static public int search(int[] nums, int target) {
+        int pivot = findPivot(nums);
+
+        if(pivot == -1) {
+            // just do normal binary search
+            return binarySearch(nums, target, 0, nums.length - 1);
+        }
+
+        if(target == nums[pivot]) {
+            return pivot;
+        }
+
+        if(target >= nums[0]) {
+            return binarySearch(nums, target, 0, pivot -1 );
+        }
+
+        return binarySearch(nums, target, pivot + 1, nums.length -1);
+    }
+
+    static public int  findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            int mid = start + ( end - start ) / 2;
+
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+
+            if (arr[start] >= arr[mid]) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    static public int binarySearch(int[] arr,int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            }
+            else if (target > arr[mid]) {
+                start = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
